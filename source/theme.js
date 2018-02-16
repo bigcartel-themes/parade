b2c4cd3ceb333ed9ca9d14f9541c75afa4bd47a2
Nick Endle <nick.endle@gmail.com>
2018-02-16 10:47:46 -0600

    add scrollmagic files

--- source/theme.js
+++ source/theme.js
@@ -1,2 +1,10 @@
-//= require_directory ./javascripts/vendor
+//= require javascripts/vendor/TweenMax.min
+//= require javascripts/vendor/ScrollMagic
+//= require javascripts/vendor/animation.gsap
+//= require javascripts/vendor/debug.addIndicators
+
+//= require javascripts/vendor/infinite-scroll.pkgd.min
+
+//= require javascripts/vendor/flickity.pkgd.min
+
 //= require javascripts/store
